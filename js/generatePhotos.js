fae47c0ca17d8ce85c622b3a0bd4465efc408170
icho-web <54773154+icho-web@users.noreply.github.com>
2020-02-25 14:55:03 +0500

    Личный проект: надо подкачаться (часть 1)

--- js/generatePhotos.js
+++ js/generatePhotos.js
@@ -1,58 +1,25 @@
 'use strict';
 
 (function () {
-  var MESSAGE = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-  var NAMES = [
-    'Сергей', 'Ирина', 'Илья', 'Вальдемар', 'Макс',
-    'Антон', 'Евгения', 'Николай', 'Дед Мороз', 'Бренность Бытия',
-    'Павел', 'Никита', 'Роман', 'Александр', 'Дмитрий',
-    'Эдвард', 'Владимир', 'Степан', 'Юлия', 'Елена',
-    'Ольга', 'Маша', 'Регина', 'Кристина', 'Мия'
-  ];
   var PHOTO_COUNT = 24;
-  var LIKES_MIN = 15;
-  var LIKES_MAX = 200;
-  var COMMENTS_MIN = 1;
-  var COMMENTS_MAX = 5;
-  var AVATAR_MIN = 1;
-  var AVATAR_MAX = 6;
+  var MIN_COMMENTS = 1;
+  var MAX_COMMENTS = 5;
   var picturesElement = document.querySelector('.pictures');
   var pictureTemplate = document.querySelector('#picture')
     .content
     .querySelector('.picture');
 
-  var generateRandomPhotos = function (count) {
-    var photos = [];
-    for (var i = 0; i < count; i++) {
-      photos.push({
-        avatar: 'img/avatar-' + window.utils.getRandomValue(AVATAR_MIN, AVATAR_MAX) + '.svg',
-        message: MESSAGE[window.utils.getRandomComment(MESSAGE)],
-        name: NAMES[window.utils.getRandomComment(NAMES)]
-      }
-      );
-    }
-    return photos;
-  };
-
-  var photosArr = generateRandomPhotos(PHOTO_COUNT);
-
-  var renderPhoto = function (photoRender) {
-    for (var i = 0; i <= photoRender.length; i++) {
+  var renderPhoto = function (photos) {
+    for (var i = 0; i <= PHOTO_COUNT; i++) {
       var pictureElement = pictureTemplate.cloneNode(true);
       var fragment = document.createDocumentFragment();
       fragment.appendChild(pictureElement);
       picturesElement.appendChild(fragment);
-      pictureElement.querySelector('.picture__img').src = 'photos/' + (i + 1) + '.jpg';
-      pictureElement.querySelector('.picture__likes').textContent = Math.round(window.utils.getRandomValue(LIKES_MIN, LIKES_MAX));
-      pictureElement.querySelector('.picture__comments').textContent = window.utils.getRandomValue(COMMENTS_MIN, COMMENTS_MAX);
+      pictureElement.querySelector('.picture__img').src = photos[i].url;
+      pictureElement.querySelector('.picture__likes').textContent = photos[i].likes;
+      pictureElement.querySelector('.picture__comments').textContent = window.utils.getRandomValue(MIN_COMMENTS, MAX_COMMENTS);
     }
   };
-  renderPhoto(photosArr);
+
+  window.backend.load(renderPhoto);
 })();
